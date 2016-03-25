28cc08af6df92697d5e7340b43a123443a2f7c1e
Alexander Tupavov <tauinbox@gmail.com>
2016-03-25 17:27:09 +0300

    add 'serve' task

--- ust/ng-m2-solution/conFusion/Gruntfile.js
+++ ust/ng-m2-solution/conFusion/Gruntfile.js
@@ -165,5 +165,8 @@ module.exports = function(grunt) {
     'filerev',
     'usemin'
 	]);
+
+	grunt.registerTask('serve',['build','connect:dist','watch']);
+	
 	grunt.registerTask('default',['build']);
 };
