9ecc65ffdcdf0e3e73e81133c1bd0f464b6f88f6
Alexander Tupavov <tauinbox@gmail.com>
2016-03-28 10:23:54 +0300

    add ngannotate() task

--- ust/ng-m2-solution/conFusion/gulpfile.js
+++ ust/ng-m2-solution/conFusion/gulpfile.js
@@ -13,6 +13,7 @@ var gulp = require('gulp'),
     changed = require('gulp-changed'),
     rev = require('gulp-rev'),
     browserSync = require('browser-sync'),
+    ngannotate = require('gulp-ng-annotate'),
     del = require('del');
 
 
@@ -40,7 +41,7 @@ gulp.task('usemin',['jshint'], function () {
   return gulp.src('./app/menu.html')
       .pipe(usemin({
         css:[minifycss(),rev()],
-        js: [uglify(),rev()]
+        js: [ngannotate(),uglify(),rev()]
       }))
       .pipe(gulp.dest('dist/'));
 });
