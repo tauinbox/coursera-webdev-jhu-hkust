43972246388de7f7976a559273213d0f3334056f
Alexander Tupavov <tauinbox@gmail.com>
2016-03-28 17:51:49 +0300

    add controllers

--- ust/ng-m2-solution/conFusion/app/scripts/app.js
+++ ust/ng-m2-solution/conFusion/app/scripts/app.js
@@ -60,13 +60,33 @@ app.controller('MenuController', ['$scope', function($scope) {
 
 app.controller('ContactController', ['$scope', function($scope) {
     $scope.feedback = {
-          mychannel:"", 
-          firstName:"", 
-          lastName:"", 
-          agree:false, 
-          email:"" };
+          mychannel: "", 
+          firstName: "", 
+          lastName: "", 
+          agree: false, 
+          email: "" };
+
+    var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+    $scope.channels = channels;
+
+    $scope.invalidChannelSelection = false;
 }]);
 
 app.controller('FeedbackController', ['$scope', function($scope) {
+    $scope.sendFeedback = function() {
+      console.log($scope.feedback);
+      if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
+        $scope.invalidChannelSelection = true;
+        console.log('incorrect');
+      }
+      else {
+        $scope.invalidChannelSelection = false;
+        $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                           agree:false, email:"" };
+        $scope.feedback.mychannel="";
 
+        $scope.feedbackForm.$setPristine();
+        console.log($scope.feedback);
+      }
+    };
 }]);
