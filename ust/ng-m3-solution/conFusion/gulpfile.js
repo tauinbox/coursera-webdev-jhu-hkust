4b0fe01edb17d7ce67d1bb373add405234fbe41e
Alexander Tupavov <tauinbox@gmail.com>
2016-03-30 14:16:10 +0300

    update watched index

--- ust/ng-m3-solution/conFusion/gulpfile.js
+++ ust/ng-m3-solution/conFusion/gulpfile.js
@@ -85,7 +85,7 @@ gulp.task('browser-sync', ['default'], function () {
    browserSync.init(files, {
       server: {
          baseDir: "dist",
-         index: "menu.html"
+         index: "index.html"
       }
    });
         // Watch any files in dist/, reload on change
