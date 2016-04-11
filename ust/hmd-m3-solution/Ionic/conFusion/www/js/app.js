c3473899fd75afe31e25360cbbd59d1d45630d2a
Alexander Tupavov <tauinbox@gmail.com>
2016-04-11 19:49:56 +0300

    add resolved resources to controllers

--- ust/hmd-m3-solution/Ionic/conFusion/www/js/app.js
+++ ust/hmd-m3-solution/Ionic/conFusion/www/js/app.js
@@ -79,7 +79,10 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.servic
     views: {
       'mainContent': {
         templateUrl: 'templates/aboutus.html',
-        controller: 'AboutController'
+        controller: 'AboutController',
+        resolve: {
+          leaders: ['corporateFactory', function(corporateFactory) { return corporateFactory.query(); }]
+        }
       }
     }
   })
@@ -112,7 +115,10 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.servic
     views: {
       'mainContent': {
         templateUrl: 'templates/menu.html',
-        controller: 'MenuController'
+        controller: 'MenuController',
+        resolve: {
+          dishes:  ['menuFactory', function(menuFactory) { return menuFactory.query(); }]
+        }            
       }
     }
   })
