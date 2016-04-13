8441a174d1740dda5c9ba3a5327c9563debd7ea4
Alexander Tupavov <tauinbox@gmail.com>
2016-04-13 18:49:18 +0300

    fix semicolon

--- ust/nd-m1-solution/node-http/server-2.js
+++ ust/nd-m1-solution/node-http/server-2.js
@@ -1,3 +1,4 @@
+
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
@@ -19,7 +20,7 @@ var server = http.createServer(function(req, res) {
       fileUrl = req.url;
     }
 
-    var filePath = path.resolve('./public'+fileUrl);
+    var filePath = path.resolve('./public' + fileUrl);
     var fileExt = path.extname(filePath);
 
     if (fileExt == '.html') {
@@ -42,8 +43,8 @@ var server = http.createServer(function(req, res) {
     res.writeHead(404, { 'Content-Type': 'text/html' });
     res.end('<html><body><h1>Error 404: ' + req.method + ' not supported</h1></body></html>');
   }
-  
-})
+
+});
 
 server.listen(port, hostname, function() {
   console.log('Server running at http://${hostname}:${port}/');
