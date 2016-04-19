3a0eed85cfd6b3af9d01e31056e87a3fe84cac2f
Alexander Tupavov <tauinbox@gmail.com>
2016-04-19 12:20:44 +0300

    modify app for using passport and jwt

--- ust/nd-m3-solution/rest-server-passport/routes/dishRouter.js
+++ ust/nd-m3-solution/rest-server-passport/routes/dishRouter.js
@@ -4,19 +4,21 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Dishes = require('../models/dishes');
 
+var Verify = require('./verify');
+
 var dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
-.get(function(req, res, next) {
+.get(Verify.verifyOrdinaryUser, function(req, res, next) {
   Dishes.find({}, function(err, dishes) {
     if (err) throw err;
     res.json(dishes);
   });
 })
 
-.post(function(req, res, next) {
+.post(Verify.verifyOrdinaryUser, function(req, res, next) {
   Dishes.create(req.body, function(err, dish) {
     if (err) throw err;
     console.log('Dish created!');
@@ -27,7 +29,7 @@ dishRouter.route('/')
   });
 })
 
-.delete(function(req, res, next) {
+.delete(Verify.verifyOrdinaryUser, function(req, res, next) {
   Dishes.remove({}, function(err, resp) {
     if (err) throw err;
     res.json(resp);
