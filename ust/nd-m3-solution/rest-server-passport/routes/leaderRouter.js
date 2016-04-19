f9428f3496bfa486677434382a699908ecfe2a83
Alexander Tupavov <tauinbox@gmail.com>
2016-04-19 10:01:46 +0300

    initial commit

---
+++ ust/nd-m3-solution/rest-server-passport/routes/leaderRouter.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+var Leadership = require('../models/leadership');
+
+var leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.get(function(req, res, next) {
+  Leadership.find({}, function(err, leaders) {
+    if (err) throw err;
+    res.json(leaders);
+  });
+})
+
+.post(function(req, res, next) {
+  Leadership.create(req.body, function(err, leader) {
+    if (err) throw err;
+    console.log('Leader created!');
+    var id = leader._id;
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Added the leader with id: ' + id);
+  });  
+})
+
+.delete(function(req, res, next) {
+  Leadership.remove({}, function(err, resp) {
+    if (err) throw err;
+    res.json(resp);
+  });
+});
+
+//////////////////////////////////////////////
+
+leaderRouter.route('/:id')
+.get(function(req, res, next) {
+  Leadership.findById(req.params.id, function(err, leader) {
+    if (err) throw err;
+    res.json(leader);
+  });
+})
+
+.put(function(req, res, next) {
+  Leadership.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true}, function(err, leader) {
+    if (err) throw err;
+    res.json(leader);
+  });
+})
+
+.delete(function(req, res, next){
+  Leadership.findByIdAndRemove(req.params.id, function(err, resp) {        
+    if (err) throw err;
+    res.json(resp);
+  });
+});
+
+module.exports = leaderRouter;
