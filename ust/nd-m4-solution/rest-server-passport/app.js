d85e99bf5af54bbb7a4a139d2b66d095d96d1cfd
Alexander Tupavov <tauinbox@gmail.com>
2016-04-20 17:21:28 +0300

    replace authentication to another module

--- ust/nd-m4-solution/rest-server-passport/app.js
+++ ust/nd-m4-solution/rest-server-passport/app.js
@@ -6,8 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
+var authenticate = require('./authenticate');
 var config = require('./config');
 
 mongoose.connect(config.mongoUrl);
@@ -47,12 +46,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// passport config
-var User = require('./models/user');
+// passport init
 app.use(passport.initialize());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
